ec59b87c475cef14bed55ba8ec28f9cdb2fe7c5d
Rainbow Classes <rainbowclasses9934@gmail.com>
2026-08-18 22:37:15 +0530

    Delete physics1.js

--- physics1.js
+++
@@ -1,9 +0,0 @@
-// फ़ाइल का नाम: physics1.js (अध्याय 1 से 5)
-window.quizDatabase = window.quizDatabase || {};
-window.quizDatabase[1] = window.quizDatabase[1] || {};
-for(let i=1; i<=5; i++) window.quizDatabase[1]["ch_"+i] = window.quizDatabase[1]["ch_"+i] || {};
-
-// 👇 यहाँ नीचे अपने सवालों का कोड (SETs) पेस्ट करें 
-window.quizDatabase[1]["ch_1"]["set_1"] = [
-    { question: "प्रश्न?", options: ["A", "B", "C", "D"], correct: 0 }
-  ]
